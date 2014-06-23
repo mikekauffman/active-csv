590d4b867724a22b0a653e13445131fc1d862346
Gerard Cote <grcote@gmail.com>
2014-06-23 15:15:19 -0600

    [#73729312] Dynamically generated attributes only return a value if the header exists.
    Signed-off-by: Mike Kauffman <mike.a.kauffman@gmail.com>

--- lib/active_csv/base.rb
+++ lib/active_csv/base.rb
@@ -7,17 +7,15 @@ module ActiveCSV
     end
 
     def method_missing(method_name)
-      if @row.first[method_name.to_s]
+      if @row.first.has_key?(method_name.to_s)
         @row.first[method_name.to_s]
-      elsif @row.first[method_name.to_s].nil?
-        nil
       else
         super
       end
     end
 
     def respond_to_missing?(method_name, include_private = false)
-      @row.first.headers.include?(method_name.to_s) || super
+      @row.first.has_key?(method_name.to_s) || super
     end
   end
 end
